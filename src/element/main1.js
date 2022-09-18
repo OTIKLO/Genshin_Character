1724a7fc3a53d1eea3f0e87dbf95454205819107
OTIKLO <aaa7284@naver.com>
2022-09-18 23:56:21 +0900

    손보는중

--- src/element/main1.js
+++ src/element/main1.js
@@ -1,16 +1,12 @@
-import bg1 from "../img/bg/bg1.png";
-import bg2 from "../img/bg/bg2.png";
-import bg3 from "../img/bg/bg3.png";
 import bg4 from "../img/bg/bg4.png";
+import "./main1.module.css";
 
 function Page1(){
-    const backgroundArr = [bg1, bg2, bg3, bg4];
-    const bgIndex = Math.floor(Math.random()*backgroundArr.length);
-    const chosenBg = backgroundArr[bgIndex];
-    document.body.style.backgroundImage = `url(${chosenBg})`;
-    
     return(
-        <div>FirstPage</div>
+        <div className="main1d">
+            <img src={bg4}></img>
+            FirstPage
+        </div>
     )
 };
 
